167653b0fe9ff649d9ebaeabc84daf9d799dbbee
JohnEdrickGador <jmgador@up.edu.ph>
2026-01-21 19:37:10 +0800

    change: removed credits and about me links in navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function Navbar() {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -9,42 +9,49 @@ function Navbar() {
 
   const burgerToggle = () => {
     setToggleMenu(!toggleMenu);
-    console.log(toggleMenu)
-  }
+    console.log(toggleMenu);
+  };
 
   const searchHandler = (e) => {
     e.preventDefault();
-    navigate(`/search/${searchInput}`)
-    window.location.reload()
-  }
+    navigate(`/search/${searchInput}`);
+    window.location.reload();
+  };
 
   const changeHandler = (e) => {
     e.preventDefault();
     setSearchInput(e.target.value);
-  }
+  };
 
   useEffect(() => {
     const changeWidth = () => {
       setScreenWidth(window.innerWidth);
-    }
-    window.addEventListener('resize', changeWidth)
-  })
-  
+    };
+    window.addEventListener("resize", changeWidth);
+  });
+
   return (
-    <nav className='navbar'>
-      <div className="website-name"><a href="/">Senpainime</a>
-        <div className='toggle-button' onClick={burgerToggle}>
+    <nav className="navbar">
+      <div className="website-name">
+        <a href="/">Senpainime</a>
+        <div className="toggle-button" onClick={burgerToggle}>
           <span className="bar"></span>
           <span className="bar"></span>
           <span className="bar"></span>
         </div>
       </div>
-      <div className='search-container'>
+      <div className="search-container">
         <form onSubmit={searchHandler}>
-          <input type="text" className="search-input" placeholder="Search..." value={searchInput} onChange={changeHandler}></input>
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search..."
+            value={searchInput}
+            onChange={changeHandler}
+          ></input>
         </form>
       </div>
-      <div className="navigation-links">
+      {/* <div className="navigation-links">
       
       {(toggleMenu || screenWidth > 800) && ( 
           <ul>
@@ -52,10 +59,9 @@ function Navbar() {
               <li><a href="/about">About Me</a></li>
           </ul>
         )}
-        </div>
-        
+        </div> */}
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
